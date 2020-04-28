6196091095f36ff48c4556efcb9708db99bf7bdd
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-04-28 04:22:29 +0100

    Adds favicon. Changes Rails version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+
+//= require rails-ujs
+//= require activestorage
+//= require_tree
+//= require turbolinks
 //= require jquery
 
 //= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
