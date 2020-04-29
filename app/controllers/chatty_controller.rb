ee2c59f43e90296b99bab737acceb53370c9524d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-04-29 23:31:32 +0100

    Adds restrictions - users' receive a notification if they try to go to the login page while logged in. Logged out users can't go to the login page - receive notification

--- app/controllers/chatty_controller.rb
+++ app/controllers/chatty_controller.rb
@@ -1,4 +1,5 @@
 class ChattyController < ApplicationController
+  before_action :require_user 
 
   def index
     @messages = Message.all
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :already_logged_in, only: [:new, :create]
 
   def new
   end 
@@ -7,19 +8,28 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have been successfully logged in"
+      flash[:success] = "Alohomora!!! You have been successfully logged in"
       redirect_to root_path
     else
-      flash.now[:error] = "Unable to log in. Please check your username and/or password"
+      flash.now[:error] = "Nox... unable to log in. Please check your username and/or password"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully been logged out"
+    flash[:success] = "Alohormora!!! You have successfully been logged out"
     redirect_to login_path
   end
+
+  private
+
+  def already_logged_in
+    if logged_in?
+      flash[:error] =  "Merlin's Beard! You are already logged in"
+      redirect_to root_path
+    end 
+  end 
 
 end
 
