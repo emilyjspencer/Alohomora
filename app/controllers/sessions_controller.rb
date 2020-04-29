17af72f36a71bfe98cf84c9a7912a5ddd25e4d2a
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-04-29 15:27:41 +0100

    Adds form helpers to login view. Adds methods to Application Controller. Adds login routes - post request. Adds create action to Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,17 @@ class SessionsController < ApplicationController
   def new
   end 
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Unable to log in. Please check your username and/or password"
+      render 'new'
+    end
+  end
+
 end
 
