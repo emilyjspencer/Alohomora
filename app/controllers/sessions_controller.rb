fac847d2e7f9a5f8149ab46db1dc901e6bcb9ab9
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-04-29 22:33:20 +0100

    Changes UI. Adds destory action in Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = "You have been successfully logged in"
       redirect_to root_path
     else
       flash.now[:error] = "Unable to log in. Please check your username and/or password"
       render 'new'
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully been logged out"
+    redirect_to login_path
   end
 
 end
